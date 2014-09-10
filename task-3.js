b1ada8c85a61aba3cd9eb1d4d366a76d5a8db82c
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-09-10 20:02:55 +0300

    added momentjs and replaced date formating

--- task-3.js
+++ task-3.js
@@ -1,21 +1,18 @@
 var file = require("./task-3_module.js");
+var moment = require('moment');
+moment.locale("fi");
 
 var filename = process.argv[2];
 
 function formatDate(ts)
 {
-    return ts.getDate() + "." + 
-        (ts.getMonth() + 1) + "." +
-        (ts.getYear() + 1900) + " " +
-        ts.getHours() + ":" +
-        ts.getMinutes() + ":" +
-        ts.getSeconds();
+  return moment(ts).format("L HH:mm:ss");
 }
 
 file.getFileDate(filename, function(stat)
 {
     if(!stat)
         return console.log("File not found!");
-    
+
     console.log(formatDate(stat));
 });
